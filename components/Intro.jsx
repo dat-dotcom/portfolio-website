cf2aa62d48596e3b95d62b1ffabfa46a44980c22
Dat Ngo <dat_ngo10@protonmail.com>
2022-11-28 14:35:48 +0800

    feat: Created Intro Component

---
+++ components/Intro.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="flex items-center justify-center flex-col text-center pt-20 pb-6">
+      <h1 className="text-4xl md:text-7xl mb-1 md:mb-3 font-bold">Dat</h1>
+
+      <p className="text-base md:text-xl mb-3 font-medium">
+        Software Developer
+      </p>
+
+      <p className="text-sm max-w-xl mb-6 font-bold">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus
+        ipsam doloremque, molestias laudantium quisquam maxime cum dolor fugiat
+        hic voluptatem soluta nulla maiores explicabo dolores ex voluptate
+        tempore exercitationem nisi sed deserunt iure, mollitia cumque possimus?
+        <br/>
+        Rem voluptate eum recusandae repudiandae expedita quos delectus maxime,
+        quod quasi accusamus laudantium, possimus harum optio beatae, vel
+        architecto. Mollitia adipisci odit quo, harum voluptatem. 
+      </p>
+    </div>
+  );
+};
+
+export default Intro;

dceadbf6c1c838b892645468035f618a2ca4ac44
Dat Ngo <dat_ngo10@protonmail.com>
2022-11-28 17:50:50 +0800

    feat: Created a timeline to showcase skills learned

---
+++ data/timeline.ts
@@ -0,0 +1,28 @@
+export default [
+  {
+    year: "Today",
+    title: "Diploma in Advance Programming",
+    duration: "Present",
+    details: "I am currently undertaking a diploma in advance programming to further my skills and knowledge to creating full scale programs",
+  },
+  {
+    year: "2022",
+    title: "Awarded a Certificate of Excellence ",
+    duration: "6 months",
+    details: "An achievement awarded to those who demonstrated continued and outstanding excellence during my study in Diploma of Advance Programming",
+  },
+  {
+    year: "2022",
+    title: "Certificate IV in Programming",
+    duration: "1 years",
+    details:
+      "This course has teach me the required knowledge to create basic programs. I learned about web development, object oriented programming, an array of programming languages, and ",
+  },
+  {
+    year: "2021",
+    title: "First line of Code",
+    duration: "The beginning",
+    details:
+      "I wrote my first line of real code that started this journey into digital craftmanship...",
+  },
+];

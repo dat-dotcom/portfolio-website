fb00694c969123ca3e445a0a3ea4a19fe6ba41c5
Dat Ngo <dat_ngo10@protonmail.com>
2022-11-29 16:05:45 +0800

    Feat: Created a contact form to notify my email

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -4,7 +4,9 @@ const Contact = () => {
   return (
     <div className='flex flex-col mb-10 mx-auto'>
         <div className="flex justify-center items-center">
-            <form action="#" method='POST' className='flex flex-col w-full md:w-7/12'>
+            <form action="https://getform.io/f/710d940e-a63a-4da6-86a4-7057b3efd6ab" 
+                  method='POST' 
+                  className='flex flex-col w-full md:w-7/12'>
                 <input  type="text" 
                         name='name'
                         placeholder='Name'
@@ -19,7 +21,7 @@ const Contact = () => {
                             placeholder='Message' 
                             rows={10} 
                             className='mb-4 p-2 bg-transparent border-2 rounded-md focus:outline-none'/>
-                <button type='button' className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md'>Work With Me</button>
+                <button type='submit' className='text-center inline-block px-8 py-3 w-max text-base font-medium rounded-md text-white bg-gradient-to-r from-yellow-500 to-pink-500 drop-shadow-md'>Work With Me</button>
             </form>
         </div>
     </div>
